fa934f4c2664b39145e012d47ffa0be29e1fe69d
Angel Qui <angelquimorita@gmail.com>
2024-08-19 00:59:25 -0700

    HTTPS suport added

--- cmd/waterwatch_back/main.go
+++ cmd/waterwatch_back/main.go
@@ -14,30 +14,41 @@ import (
 
 type Config struct {
 	Port string
+	Env  string // Environment variable to differentiate between production and development
 }
 
 type Application struct {
-	Config Config
-	Models models.Models
+	Config      Config
+	Models      models.Models
 	Controllers controllers.Controllers
 }
 
-var port = os.Getenv("PORT")
+var (
+	port = os.Getenv("PORT")
+	env  = os.Getenv("ENV") // Environment variable to check if it's production
+)
 
 func (app *Application) Serve() error {
-	fmt.Println("API listening on port ", app.Config.Port)
+	fmt.Println("API listening on port", app.Config.Port)
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", app.Config.Port),
 		Handler: router.Routes(&app.Controllers),
 	}
 
-	return srv.ListenAndServe()
+	if app.Config.Env == "production" {
+		// Use HTTPS in production with the Let's Encrypt certificate
+		return srv.ListenAndServeTLS("/etc/letsencrypt/live/angelqui.xyz/fullchain.pem", "/etc/letsencrypt/live/angelqui.xyz/privkey.pem")
+	} else {
+		// Use HTTP for development
+		return srv.ListenAndServe()
+	}
 }
 
 func main() {
 	var cfg Config
 	cfg.Port = port
+	cfg.Env = env
 
 	dsn := os.Getenv("DSN")
 	dbConn, err := database.ConnectPostgres(dsn)
@@ -48,16 +59,16 @@ func main() {
 	defer dbConn.DB.Close()
 
 	models := models.New(dbConn.DB)
-  controllers := controllers.New(dbConn.DB)
+	controllers := controllers.New(dbConn.DB)
 
 	app := &Application{
-		Config: cfg,
-		Models: models, // my homies and I love DI
+		Config:      cfg,
+		Models:      models,
 		Controllers: controllers,
 	}
 
 	err = app.Serve()
 	if err != nil {
-		log.Fatal("Error creating app", err)
+		log.Fatal("Error starting server", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -8,3 +8,8 @@ require (
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.25.0
 )
+
+require (
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/gorilla/handlers v1.5.2 // indirect
+)
